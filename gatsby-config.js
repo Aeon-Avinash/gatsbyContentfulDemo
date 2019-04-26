a3015c18f36300e8a46ca6df6a76a430e93af35a
Aeon Avinash <aeon.b.avinash@gmail.com>
2019-04-26 08:59:51 +0530

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,20 @@
+const dotenv = require('dotenv');
+
+if(process.env.ENVIRONMENT !== 'production') {
+  dotenv.config();
+}
+
+const { spaceId, accessToken } = process.env;
+
+module.exports = {
+  plugins: [
+    "gatsby-transformer-remark",
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        spaceId,
+        accessToken
+      }
+    }
+  ]
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,38 @@
-import React from "react"
+import React from "react";
+import { graphql } from "gatsby";
 
-export default () => <div>Hello world!</div>
+const HomePage = ({data}) => { 
+  const {title, image, date, content} = data.contentfulHomePage
+  return (
+    <div>
+      <h1>{title}</h1>
+      <small>Created on {date}</small>
+      <img src={image.file.url} alt={image.title} />
+      <div dangerouslySetInnerHTML={{ __html: content.childMarkdownRemark.html }} />
+    </div>
+  );
+}
+
+export const query = graphql`
+  query {
+    contentfulHomePage {
+      createdAt
+      contentful_id
+      title
+      date
+      content {
+        childMarkdownRemark {
+          html
+        }
+      }
+      image {
+        file {
+          url
+        }
+        title
+      }
+    }
+  }
+`;
+
+export default HomePage;
